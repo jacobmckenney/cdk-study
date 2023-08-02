0fb0b03f2bf85383fbcfb3a5d61ab593dd8214e4
Jacob McKenney <jacobmckenney@Jacobs-macbook.lan>
2023-08-01 23:04:35 -0700

    initial ecr push github action

--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -1,19 +1,28 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as sqs from "aws-cdk-lib/aws-sqs";
+import { Stack, StackProps } from "aws-cdk-lib";
+import * as ECS from "aws-cdk-lib/aws-ecs";
+import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
 import { Construct } from "constructs";
 
 export class AwsStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const queue = new sqs.Queue(this, "AwsQueue", {
-            visibilityTimeout: Duration.seconds(300),
+        const cluster = new ECS.Cluster(this, "jacob-testing-ecs-cluster");
+
+        // // Build Docker image and push it to ECR
+        // const image = new DockerImageAsset(this, "jacob-testing", {
+        //     directory: "./",
+        // });
+
+        // Create ECS service with previously built and pushed image
+        new ApplicationLoadBalancedFargateService(this, "jacob-testing-ecs-service", {
+            cluster,
+            taskImageOptions: {
+                image: ECS.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+                containerPort: 80,
+            },
+            listenerPort: 80,
+            publicLoadBalancer: true,
         });
-
-        const topic = new sns.Topic(this, "AwsTopic");
-
-        topic.addSubscription(new subs.SqsSubscription(queue));
     }
 }
